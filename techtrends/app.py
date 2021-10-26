be8c7cc42f8ae9b4a49a04cbdd81e093be390db7
hilyas <hilyas@users.noreply.github.com>
2021-10-26 19:45:10 -0400

    add metrics and health eps, and logging

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,13 +1,27 @@
 import sqlite3
+import logging
+
+from datetime import datetime
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+TOTAL_DB_CONNECTIONS = 0
+
+logging.basicConfig(filename='app.log',
+                    encoding='utf-8', 
+                    level=logging.DEBUG)
+
+def log_print(message):
+    logging.debug(f"{datetime.now()}, {message}")
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global TOTAL_DB_CONNECTIONS
+    TOTAL_DB_CONNECTIONS += 1
     return connection
 
 # Function to get a post using its ID
@@ -18,9 +32,29 @@ def get_post(post_id):
     connection.close()
     return post
 
+def get_total_posts():
+    connection = get_db_connection()
+    total_posts = len(connection.execute('SELECT * FROM posts').fetchall())
+    connection.close()
+    return total_posts
+
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route("/healthz")
+def status():
+    resp_data = {"result":"OK - healthy"}
+    log_print("/healthz page retrieved!")
+    return jsonify(resp_data), 200
+
+@app.route("/metrics")
+def metrics():
+    resp_data = {"status":"success",
+                "code":0,
+                "data":{"db_connection_count":TOTAL_DB_CONNECTIONS,"post_count":get_total_posts()}}
+    log_print("/metrics page retrieved!")
+    return jsonify(resp_data), 200
 
 # Define the main route of the web application 
 @app.route('/')
@@ -36,13 +70,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      log_print("A non-existing article was accessed! 404 error!")
       return render_template('404.html'), 404
     else:
+      log_print(f"Article '{post['title']}' retrieved!")
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_print("/about page retrieved!")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -61,6 +98,7 @@ def create():
             connection.commit()
             connection.close()
 
+            log_print(f"Article '{title}' created!")
             return redirect(url_for('index'))
 
     return render_template('create.html')
